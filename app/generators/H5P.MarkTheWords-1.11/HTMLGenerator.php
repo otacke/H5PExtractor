41436a20f0e8d3b15c2f46d3dfdee1543e0e1e9b
Oliver Tacke <o.tacke@posteo.de>
2024-04-19 20:00:57 +0200

    Add generators for MarkTheWords

---
+++ app/generators/H5P.MarkTheWords-1.11/HTMLGenerator.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://todo
+ */
+
+namespace H5PExtractor;
+
+require_once __DIR__ . '/../HtmlGeneratorInterface.php';
+
+/**
+ * Class for generating HTML for H5P.MarkTheWords-1.11.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://todo
+ */
+class HtmlGeneratorMarkTheWords_1_11 implements HtmlGeneratorInterface
+{
+
+    private function _getLinesContent($input)
+    {
+        if (strpos($input, '<p>') === false) {
+            return array($input);
+        }
+
+        $input = str_replace(array("\r", "\n", "\r\n"), '', $input);
+
+        $pattern = '/<p>(.*?)<\/p>/';
+        preg_match_all($pattern, $input, $matches);
+        return $matches[1];
+    }
+
+    private function _interpretText($input)
+    {
+        $output = str_replace('<br>', "\n\n", $input);
+
+        // Remove asterisks as required
+        $pattern = '/\*(\w+\**)\*/';
+        $callback = function ($matches)
+        {
+            return str_replace('**', '*', $matches[1]);
+        };
+        $output = preg_replace_callback($pattern, $callback, $output);
+
+        $output = htmlspecialchars_decode($output);
+
+        // Sandwich each word with span, but keep HTML tags
+        $pattern = '/(?:<[^>]+>)|(\b(?:\w+|-|–)+\b)/';
+        $callback = function ($matches)
+        {
+            $match = $matches[1] ?? htmlspecialchars($matches[0]);
+            return '<span role="option">' . $match . "</span>";
+        };
+
+        $output = preg_replace_callback($pattern, $callback, $output);
+
+        // Asterisks that were inside asterisks may belong to the word
+        $output = str_replace('</span>*', '*</span>', $output);
+        $output = str_replace('*<span role="option">', '<span role="option">*', $output);
+
+        // Remove gaps in between tags
+        $output = str_replace('&gt;</span><span role="option">', '&gt;', $output);
+        $output = str_replace('</span><span role="option">&lt;', '&lt;', $output);
+
+        $output = str_replace("\n\n", '<br>', $output);
+
+        return $output;
+    }
+
+    /**
+     * Create the HTML for the given H5P content type.
+     *
+     * @param array             $params Parameters.
+     * @param HtmlGeneratorMain $main   The main HTML generator.
+     *
+     * @return string The HTML for the H5P content type.
+     */
+    public function get($params, $main)
+    {
+        $contentParams = $params['params'];
+
+        $html = $params['container'];
+
+        preg_match('/<([a-zA-Z]+)(?:\s+[^>]*)?>/', $html, $matches);
+        $tag_name = isset($matches[1]) ? $matches[1] : '';
+
+        $htmlClosing = ($tag_name) ? '</' . $tag_name . '>' : '</div>';
+
+        /* In theory, one could derive this automatically and do in the parent,
+         * but content types may not follow the common schema to define the main
+         * class name.
+         */
+        $html = str_replace('h5pClassName', 'h5p-mark-the-words', $html);
+
+        if (isset($contentParams['media']['type'])) {
+            $html .= $main->renderH5PQuestionMedia(
+                $contentParams['media']['type']
+            );
+        }
+
+        $html .= '<div class="h5p-question-introduction">';
+        $html .= '<div>' . ($contentParams['taskDescription'] ?? ''). '</div>';
+        $html .= '</div>';
+
+        $html .= '<div class="h5p-question-content h5p-word">';
+        $html .= '<div class="h5p-word-inner">';
+        $html .= '<div class="h5p-word-selectable-words">';
+
+        $textField = $contentParams['textField'] ?? '';
+
+        $lines = $this->_getLinesContent($textField);
+        foreach ($lines as $line) {
+            $line = $this->_interpretText($line);
+            $line = '<p>' . $line . '</p>';
+
+            $html .= $line;
+        }
+
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '</div>';
+
+        $html .= $htmlClosing;
+
+        return $html;
+    }
+}
---
+++ app/generators/H5P.MarkTheWords-1.11/PlainTextGenerator.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://todo
+ */
+
+namespace H5PExtractor;
+
+require_once __DIR__ . '/../PlainTextGeneratorInterface.php';
+require_once __DIR__ . '/../../utils/TextUtils.php';
+
+/**
+ * Class for generating HTML for H5P.MarkTheWords-1.11.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://todo
+ */
+class PlainTextGeneratorMarkTheWords_1_11 implements PlainTextGeneratorInterface
+{
+    private function _getLinesContent($input)
+    {
+        if (strpos($input, '<p>') === false) {
+            return array($input);
+        }
+
+        $input = str_replace(array("\r", "\n", "\r\n"), '', $input);
+
+        $pattern = '/<p>(.*?)<\/p>/';
+        preg_match_all($pattern, $input, $matches);
+        return $matches[1];
+    }
+
+    private function _interpretText($input)
+    {
+        // Remove asterisks as required
+        $pattern = '/\*(\w+\**)\*/';
+        $callback = function ($matches)
+        {
+            return str_replace('**', '*', $matches[1]);
+        };
+        $output = preg_replace_callback($pattern, $callback, $input);
+
+        $output = htmlspecialchars_decode($output);
+
+        $nbsp_before = uniqid();
+        $nbsp_after = uniqid();
+
+        // Sandwich each word with non-breaking spaces, keep HTML tags together
+        $pattern = '/(?:<[^>]+>)|(\b(?:\w+|-|–)+\**\b)/';
+        $callback = function ($matches) use ($nbsp_before, $nbsp_after)
+        {
+            $match = $matches[1] ?? $matches[0];
+            return $nbsp_before . $match . $nbsp_after;
+        };
+        $output = preg_replace_callback($pattern, $callback, $output);
+
+        // Asterisks that were inside asterisks may belong to the word
+        $output = str_replace('*' . $nbsp_before, $nbsp_before . '*', $output);
+        $output = str_replace($nbsp_after . '*', '*' .$nbsp_after, $output);
+
+        // Remove gaps in between tags
+        $output = str_replace('>' . $nbsp_after . $nbsp_before, '>', $output);
+        $output = str_replace($nbsp_after . $nbsp_before . '<', '<', $output);
+
+        $output = str_replace($nbsp_before, "\u{00A0}", $output);
+        $output = str_replace($nbsp_after, "\u{00A0}", $output);
+
+        return $output;
+    }
+
+    /**
+     * Create the HTML for the given H5P content type.
+     *
+     * @param array                  $params Parameters.
+     * @param PlainTextGeneratorMain $main   The main HTML generator.
+     *
+     * @return string The HTML for the H5P content type.
+     */
+    public function get($params, $main)
+    {
+        $contentParams = $params['params'];
+
+        $text = $params['container'];
+
+        if (isset($contentParams['media']['type'])) {
+            $text .= $main->renderH5PQuestionMedia(
+                $contentParams['media']['type']
+            );
+        }
+
+        $text .= TextUtils::htmlToText(($contentParams['taskDescription'] ?? ''));
+
+        $textField = $contentParams['textField'] ?? '';
+        $lines = $this->_getLinesContent($textField);
+
+        foreach ($lines as $line) {
+            $line = str_replace('<br>', "\n", $line);
+
+            $text .= $this->_interpretText($line);
+            $text .= "\n\n";
+        }
+
+        return trim($text);
+    }
+}
