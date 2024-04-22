824bca7d7883777aa17cc2f456e43df2be9a15a7
Oliver Tacke <o.tacke@posteo.de>
2024-04-22 21:07:51 +0200

    Add Audio generators

---
+++ app/generators/H5P.Audio-1.5/HTMLGenerator.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+
+namespace H5PExtractor;
+
+/**
+ * Class for generating HTML for H5P.Audio-1.5.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+class HtmlGeneratorAudioMajor1Minor5 implements HtmlGeneratorInterface
+{
+    /**
+     * Create the HTML for the given H5P content type.
+     *
+     * @param array             $params Parameters.
+     * @param HtmlGeneratorMain $main   The main HTML generator.
+     *
+     * @return string The HTML for the H5P content type.
+     */
+    public function get($params, $main)
+    {
+        $contentParams = $params['params'];
+
+        $html = $params['container'];
+
+        preg_match('/<([a-zA-Z]+)(?:\s+[^>]*)?>/', $html, $matches);
+        $tag_name = isset($matches[1]) ? $matches[1] : '';
+
+        if ($params['container'] === '') {
+            $htmlClosing = '';
+        } else {
+            $htmlClosing = ($tag_name) ? '</' . $tag_name . '>' : '</div>';
+        }
+
+        /* In theory, one could derive this automatically and do in the parent,
+         * but content types may not follow the common schema to define the main
+         * class name.
+         */
+        $html = str_replace('h5pClassName', 'h5p-audio-wrapper', $html);
+
+        if ($contentParams['playerMode'] === 'minimalistic') {
+            $html .= '<div class="h5p-audio-inner">';
+            $html .= '<button';
+            if ($contentParams['fitToWrapper'] !== false) {
+                $html .= ' style="width: 100%; height: 100%;"';
+            }
+            $html .= ' class="h5p-audio-minimal-button h5p-audio-minimal-play"';
+            $html .= '/>';
+            $html .= '</div>';
+        } else if ($contentParams['playerMode'] === 'full') {
+            $imagePath = __DIR__ . '/../../assets/placeholder-audio.svg';
+
+            $html .= '<img' .
+                ' src="' . FileUtils::fileToBase64($imagePath) . '"' .
+                ' style="width: 100%;"' .
+                '>';
+        } else if ($contentParams['playerMode'] === 'transparent') {
+            // Intenionally left empty
+        }
+
+        $html .= $htmlClosing;
+
+        return $html;
+    }
+}
---
+++ app/generators/H5P.Audio-1.5/PlainTextGenerator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+
+namespace H5PExtractor;
+
+/**
+ * Class for generating plain text for H5P.Audio-1.5.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+class PlainTextGeneratorAudioMajor1Minor5 implements PlainTextGeneratorInterface
+{
+    /**
+     * Create the plain text for the given H5P content type.
+     *
+     * @param array                  $params Parameters.
+     * @param PlainTextGeneratorMain $main   The main plain text generator.
+     *
+     * @return string The plain text for the H5P content type.
+     */
+    public function get($params, $main)
+    {
+        $contentParams = $params['params'];
+        $metadata = $params['metadata'] ?? [];
+
+        $text = $params['container'];
+
+        $title = '';
+        if (!empty($metadata['a11yTitle'])) {
+            $title .= ': ' . $metadata['a11yTitle'];
+        } elseif (!empty($metadata['title'])) {
+            $title .= ': ' . $metadata['title'];
+        }
+
+        $text .= 'Audio' . $title . "\n\n";
+
+        return $text;
+    }
+}
