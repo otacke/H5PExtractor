3dbd1f65e212a3599b97f8357f1fb1813e4590fd
Oliver Tacke <o.tacke@posteo.de>
2024-04-24 18:47:33 +0200

    Improve htmlToText

--- app/utils/TextUtils.php
+++ app/utils/TextUtils.php
@@ -33,6 +33,8 @@ class TextUtils
      */
     public static function htmlToText($string)
     {
+        $string = preg_replace('/\r?\n/', '', $string);
+
         // Remove <p> tags
         $string = preg_replace('/<p[^>]*>/', "", $string);
 
@@ -49,11 +51,35 @@ class TextUtils
             $string
         );
 
+        // Replace <strong> tags with Markdown bold syntax
+        $string = preg_replace('/<strong>(.*?)<\/strong>/', '**$1**', $string);
+
+        // Replace <em> tags with Markdown italic syntax
+        $string = preg_replace('/<em>(.*?)<\/em>/', '*$1*', $string);
+
+        // Replace <ul><li> tags with Markdown unordered list syntax
+        $string = preg_replace_callback('/<ul>(.*?)<\/ul>/', function($matches) {
+            $listItems = preg_replace('/<li>(.*?)<\/li>/', "- $1\n", $matches[1]);
+            return $listItems;
+        }, $string);
+
+        // Replace <ol><li> tags with Markdown ordered list syntax
+        $string = preg_replace_callback('/<ol>(.*?)<\/ol>/', function($matches) {
+            $listItems = preg_replace_callback('/<li>(.*?)<\/li>/', function($matches) {
+                static $counter = 1;
+                return "{$counter}. {$matches[1]}\n";
+            }, $matches[1]);
+            return $listItems;
+        }, $string);
+
         // Remove all other HTML tags
         $string = preg_replace('/<[^>]*>/', '', $string);
 
         // Limit consecutive line breaks to 2
         $string = preg_replace('/(\n{3,})/', "\n\n", $string);
+
+        error_log($string);
+
         return $string;
     }
 }
