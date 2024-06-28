e3bb33185e51e8cc89ee89224b1d28cbeb1b628b
Oliver Tacke <o.tacke@posteo.de>
2024-06-28 14:23:34 +0200

    Add generators for H5P.Text

---
+++ app/generators/H5P.Text-1.1/HTMLGenerator.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+
+namespace H5PExtractor;
+
+/**
+ * Class for generating HTML for H5P.Text-1.1.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+class HtmlGeneratorTextMajor1Minor1 extends Generator implements GeneratorInterface
+{
+
+    /**
+     * Constructor.
+     *
+     * @param HTMLGeneratorMain $main The main HTML generator.
+     */
+    public function __construct($params, $contentId, $extras)
+    {
+        parent::__construct($params, $contentId, $extras);
+    }
+
+    /**
+     * Create the HTML for the given H5P content type.
+     *
+     * @param string $container Container for H5P content.
+     *
+     * @return string The HTML for the H5P content type.
+     */
+    public function attach(&$container)
+    {
+        $htmlClosing = TextUtils::getClosingTag($container);
+
+        /* In theory, one could derive this automatically and do in the parent,
+         * but content types may not follow the common schema to define the main
+         * class name.
+         */
+        $container = str_replace('h5pClassName', 'h5p-text', $container);
+
+        if (isset($this->params['text'])) {
+            $container .= $this->params['text'];
+        }
+
+        $container .= $htmlClosing;
+    }
+}
---
+++ app/generators/H5P.Text-1.1/PlainTextGenerator.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Proof of concept code for extracting and displaying H5P content server-side.
+ *
+ * PHP version 8
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+
+namespace H5PExtractor;
+
+/**
+ * Class for generating HTML for H5P.AdvancedText-1.1.
+ *
+ * @category Tool
+ * @package  H5PExtractor
+ * @author   Oliver Tacke <oliver@snordian.de>
+ * @license  MIT License
+ * @link     https://github.com/otacke/H5PExtractor
+ */
+class PlainTextGeneratorAdvancedTextMajor1Minor1 extends Generator implements GeneratorInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param array $params     Parameters.
+     * @param int   $contentId  Content ID.
+     * @param array $extras     Extras.
+     */
+    public function __construct($params, $contentId, $extras)
+    {
+        parent::__construct($params, $contentId, $extras);
+    }
+
+    /**
+     * Create the HTML for the given H5P content type.
+     *
+     * @param string $container Container for H5P content.
+     *
+     * @return string The HTML for the H5P content type.
+     */
+    public function attach(&$container)
+    {
+        if (isset($this->params['text'])) {
+            $container .= TextUtils::htmlToText($this->params['text']);
+        }
+
+        $container = trim($container);
+    }
+}
